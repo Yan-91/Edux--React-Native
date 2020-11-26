19f7de9938e9f7329bdb7e4a71f303849019e049
ViniciusMirandaDev <vinimb07@gmail.com>
2020-11-26 00:05:56 -0300

    Login precisando de ajustes

--- edux-mobile-react-native/App.js
+++ edux-mobile-react-native/App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './pages/Login';
+
+// Navigations
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './pages/Home';
+
+const Drawer = createDrawerNavigator();
+const Stack  = createStackNavigator();
+
+const Autenticado = () => {
+  return(
+    <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
