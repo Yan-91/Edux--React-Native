52a44427c227330ee1ca85bbaf2d550e3d2f2d57
Bruno-VL <68872116+Bruno-VL@users.noreply.github.com>
2020-11-26 23:10:48 -0300

    Update App.js

--- edux-mobile-react-native/App.js
+++ edux-mobile-react-native/App.js
@@ -3,23 +3,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './pages/Login';
 
-// Navigations
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Home from './pages/Home';
-
-const Drawer = createDrawerNavigator();
-const Stack  = createStackNavigator();
-
-const Autenticado = () => {
-  return(
-    <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
-    </Drawer.Navigator>
-  )
-}
-
 import Home from './pages/Home'
 import Indisponivel from './pages/Indisponivel'
 
