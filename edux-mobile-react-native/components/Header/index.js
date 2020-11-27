f04cb59d71588f43673dc1667a72e8e4add1d25e
Bruno Vidal <brunovidal8921@gmail.com>
2020-11-26 23:05:50 -0300

    Header home parte 2: o infernoa

--- edux-mobile-react-native/components/Header/index.js
+++ edux-mobile-react-native/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, ThemeProvider, Header, Image } from 'react-native-elements';
-import logo from '../../assets/ImagemEdux.png'
+import logo2 from '../../assets/ImagemEdux.png'
 
 const Headers = () => {
     return (
@@ -10,8 +10,8 @@ const Headers = () => {
                 containerStyle={{
                     backgroundColor: '#3D6DCC',
                     justifyContent: 'space-around',}}
-                leftComponent={ <Image source={{ logo }} style={{ width: 155, height: 50 }}/>  }
-                rightComponent={ <Button style={{ icon: 'home', color: '#fff' }}/>}
+                // leftComponent={ <Image source={{ logo }} style={{ width: 155, height: 50 }}/>  }
+                // rightComponent={ <Button style={{ icon: 'home', color: '#fff' }}/>}
             >
             </Header>
         </View>
