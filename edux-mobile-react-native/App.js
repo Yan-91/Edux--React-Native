62bc39ac3132c130e7eef73d8335a8f0645aa839
ViniciusMirandaDev <vinimb07@gmail.com>
2020-12-02 15:48:28 -0300

    Alterações

--- edux-mobile-react-native/App.js
+++ edux-mobile-react-native/App.js
@@ -31,7 +31,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown : false }}>
-        {/* <Stack.Screen name="Login" component={Login} />  */}
+        {<Stack.Screen name="Login" component={Login} />}
         <Stack.Screen name="Autenticado" component={Autenticado} />
       </Stack.Navigator>
   </NavigationContainer>
